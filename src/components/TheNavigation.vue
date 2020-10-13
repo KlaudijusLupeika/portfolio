c3b3a76c2f853e3552a3e3e189a87ccb09ab1894
itstoocloudy <59293214+itstoocloudy@users.noreply.github.com>
2020-10-13 15:33:17 +0100

    Update TheNavigation.vue
    resume link updated

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -12,7 +12,7 @@
         >
       </li>
       <li>
-        <a target="_blank" href="//drive.google.com/file/d/1PPw5rKiROW0hL46R7u-gKIf5chvHRwve/view?usp=sharing"
+        <a target="_blank" href="//drive.google.com/uc?export=download&id=1PY84M-ZYxGSiOBHXz1zVtYIz4X6lXHVB"
           ><hr />
           Resume</a
         >
